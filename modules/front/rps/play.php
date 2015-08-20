a86b91549b0f82fa8090326faba56ef3f6228725
Makoto <FujiMakoto@users.noreply.github.com>
2015-08-20 06:38:02 -0400

    Konami code

--- modules/front/rps/play.php
+++ modules/front/rps/play.php
@@ -19,9 +19,14 @@ class _play extends \IPS\Dispatcher\Controller
 	const PAPER 	= 'paper';
 	const SCISSORS	= 'scissors';
 
+	/**
+	 * @brief	Base URL
+	 */
+	protected static $baseUrl = 'app=rps&module=rps&controller=play';
+
 	private function _cheater()
 	{
-		return ( in_array( 'rps_cheater', \IPS\Request::i()->cookie )
+		return ( !empty( \IPS\Request::i()->cookie['rps_cheater'] )
 			and \IPS\Request::i()->cookie['rps_cheater'] == 'RWwgUHN5IENvbmdyb28=' );
 	}
 
@@ -50,26 +55,24 @@ class _play extends \IPS\Dispatcher\Controller
 	 */
 	public function execute()
 	{
+		\IPS\Output::i()->title = \IPS\Member::loggedIn()->language()->addToStack( 'module__rps_rps' );
+
 		/* We need FontAwesome 4.4+ for the Roshambo icons */
 		\IPS\Output::i()->cssFiles = array_merge( \IPS\Output::i()->cssFiles, array(
 			\IPS\Http\Url::external( 'https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css' )
 		) );
 		\IPS\Output::i()->cssFiles = array_merge( \IPS\Output::i()->cssFiles, \IPS\Theme::i()->css( 'play.css' ) );
+		\IPS\Output::i()->jsFiles = array_merge( \IPS\Output::i()->jsFiles, \IPS\Output::i()->js( 'front_play.js', 'rps' ) );
 
 		parent::execute();
 	}
 
 	/**
-	 * ...
+	 * Game start!
 	 *
 	 * @return	void
 	 */
 	protected function manage()
-	{
-		return \IPS\Output::i()->output = \IPS\Theme::i()->getTemplate( 'rps' )->intro();
-	}
-
-	public function play()
 	{
 		return \IPS\Output::i()->output = \IPS\Theme::i()->getTemplate( 'rps' )->select( $this->_scoreboard() );
 	}
@@ -81,6 +84,8 @@ class _play extends \IPS\Dispatcher\Controller
 	 */
 	public function shoot()
 	{
+		\IPS\Session::i()->csrfCheck();
+
 		/* Key beats value */
 		$validMoves = array(
 			static::ROCK => static::SCISSORS,
@@ -97,7 +102,7 @@ class _play extends \IPS\Dispatcher\Controller
 
 		/* Did we win? */
 		$winner = $winner = $chickenDinner = ( ( $playerMove !== $validMoves[ $compMove ] ) or $this->_cheater() );
-		$draw = ( $playerMove === $compMove );
+		$draw = ( ( !$this->_cheater() ) and ( $playerMove === $compMove ) );
 
 		/* Update the scoreboard */
 		$scoreboard = $this->_scoreboard();
@@ -118,32 +123,49 @@ class _play extends \IPS\Dispatcher\Controller
 	}
 
 	/**
+	 * Reset the scoreboard
+	 */
+	public function resetScore()
+	{
+		\IPS\Session::i()->csrfCheck();
+
+		\IPS\Request::i()->setCookie( 'rps_scoreboard', json_encode(
+			array( 'wins' => 0, 'losses' => 0, 'draws' => 0 )
+		) );
+
+		if ( \IPS\Request::i()->isAjax() ) {
+			return \IPS\Output::i()->json( 'rps_score_reset' );
+		}
+		return \IPS\Output::i()->redirect( \IPS\Http\Url::internal( static::$baseUrl ), 'rps_score_reset', 302 );
+	}
+
+	/**
 	 * @link https://www.youtube.com/watch?v=MjmnknmC8H0
 	 *
 	 * @return	void
 	 */
 	public function konami()
 	{
-		if ( \IPS\Request::i()->code != 'RWwgUHN5IENvbmdyb28' ) {
+		if ( \IPS\Request::i()->code != 'RWwgUHN5IENvbmdyb28=' ) {
 			return \IPS\Output::i()->error( 'page_not_found', '2S100/5', 404 );
 		}
 		\IPS\Session::i()->csrfCheck();
 
 		/* ^('-')^ ^('-')^ v('-')v v('-')v <('-'<) (>'-')> <('-'<) (>'-')> B A */
-		if ( in_array( 'rps_cheater', \IPS\Request::i()->cookie ) )
+		if ( isset( \IPS\Request::i()->cookie['rps_cheater'] ) )
 		{
 			$message = 'Cheat mode disabled!';
-			unset( \IPS\Request::i()->cookie['rps_cheater'] );
+			\IPS\Request::i()->setCookie( 'rps_cheater', null );
 		}
 		else
 		{
 			$message = 'Cheat mode enabled!';
-			\IPS\Request::i()->setCookie( 'rps_cheater', 'RWwgUHN5IENvbmdyb28' );
+			\IPS\Request::i()->setCookie( 'rps_cheater', 'RWwgUHN5IENvbmdyb28=' );
 		}
 
 		if ( \IPS\Request::i()->isAjax() ) {
 			return \IPS\Output::i()->json( $message );
 		}
-		return \IPS\Output::i()->redirect( 'app=rps&module=rps&controller=play', $message, 302 );
+		return \IPS\Output::i()->redirect( \IPS\Http\Url::internal( static::$baseUrl ), $message, 302 );
 	}
 }
