26bfa52efdc5d58b752f8f9c94e9dad0e16c9568
Makoto <FujiMakoto@users.noreply.github.com>
2015-08-20 06:38:47 -0400

    Template links and module language string

--- dev/lang.php
+++ dev/lang.php
@@ -1,17 +1,20 @@
 <?php
 
 $lang = array(
-	'__app_rps'	=> "Rock Paper Scissors",
+	'__app_rps'			=> "Rock Paper Scissors",
+	'module__rps_rps'	=> 'Rock Paper Scissors',
 
 	/* Sidebar */
 	'rps_scoreboard'	=> 'Scoreboard',
 	'rps_computer'		=> 'Computer',
 	'rps_draws'			=> 'Draws',
 	'rps_reset'			=> 'Reset Score',
+	'rps_score_reset'	=> 'Scoreboard reset',
 
 	/* Game result */
 	'rps_you_win'	=> 'You win!',
 	'rps_you_lose'	=> 'You lose!',
+	'rps_you_draw'	=> "It's a draw!",
 
 	'rps_comp_played'	=> 'Computer played',
 	'rps_you_played'	=> 'You played',
