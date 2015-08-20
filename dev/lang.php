2b8da3f1690bd6d65ed87c8f10ca82bc8b679dbd
Makoto <FujiMakoto@users.noreply.github.com>
2015-08-20 03:12:46 -0400

    Language

--- dev/lang.php
+++ dev/lang.php
@@ -1,5 +1,20 @@
 <?php
 
 $lang = array(
-	'__app_rps'	=> "Rock Paper Scissors"
+	'__app_rps'	=> "Rock Paper Scissors",
+
+	/* Sidebar */
+	'rps_scoreboard'	=> 'Scoreboard',
+	'rps_computer'		=> 'Computer',
+	'rps_draws'			=> 'Draws',
+	'rps_reset'			=> 'Reset Score',
+
+	/* Game result */
+	'rps_you_win'	=> 'You win!',
+	'rps_you_lose'	=> 'You lose!',
+
+	'rps_comp_played'	=> 'Computer played',
+	'rps_you_played'	=> 'You played',
+
+	'rps_play_again'	=> 'Play again'
 );
